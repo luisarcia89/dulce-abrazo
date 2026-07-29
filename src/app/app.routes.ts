90535cf912c61bc6921842118f6f286b0d596705
luisarcia89 <luisosneider@gmail.com>
2026-07-28 19:22:56 -0500

    Landing Dulce Abrazo con Angular, rutas y Bootstrap

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { MisionComponent } from './pages/mision/mision.component';
+import { VisionComponent } from './pages/vision/vision.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'mision', component: MisionComponent },
+  { path: 'vision', component: VisionComponent },
+];
